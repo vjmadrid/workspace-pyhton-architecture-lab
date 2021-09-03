b8989abbbf125ac46c8298ccd3ae5120c695bc95
vjmadrid <vjmadrid@atsistemas.com>
2021-09-03 12:30:04 +0200

    Prepare

--- acme-core/README.md
+++ acme-core/README.md
@@ -1,8 +1,28 @@
 # acme-core
 
-python setup.py develop
+This project represents a architecture library (dependency) related to the **elements common to any project** to develop the different parts in a homogeneus way
 
-python setup.py develop --uninstall
+This library stands out for:
+
+* Define xxxxx
 
 
-python setup.py install
+curl.trillworks.com
+
+
+
+## Technological Stack
+
+* [Python](https://www.python.org/)
+
+Dependencies with architecture projects
+
+* **acme-test** [0.0.1] : Architecture library for testing used in the test environment -> Custom Library [Documentation](XXXX)
+
+
+Third Party
+
+* **python-dateutil** [2.8.2] : Framework Extensions to the standard Python datetime module -> [Pypi](https://pypi.org/project/python-dateutil/) [Documentation](https://github.com/dateutil/dateutil)
+
+  * Includes :
+    * six
--- acme-core/setup.py
+++ acme-core/setup.py
@@ -1,15 +1,133 @@
-from setuptools import setup, find_packages
+# -*- coding: utf-8 -*-
+
+
+import io
+import os
+import sys
+
+from shutil import rmtree
+from setuptools import setup, find_packages, Command
+
+
+# Project path
+path = os.path.abspath(os.path.dirname(__file__))
+
+
+# Package meta-data default values
+NAME = "acme-core"
+DESCRIPTION = "Architecture library (dependency) related with core to develop the different parts in \
+    a homogeneus way"
+URL = "https://github.com/vjmadrid/workspace-python-architecture-lab/tree/main/acme-common"
+EMAIL = "vjmadrid"
+AUTHOR = "Víctor Madrid"
+REQUIRES_PYTHON = ">=3.9.0"
+VERSION = "0.0.1"
+# VERSION = False
+PACKAGES_EXCLUDE = ["tests", "*.tests", "*.tests.*", "tests.*", "docs", "examples"]
+REQUIREMENTS_FILE = "dev-requirements.txt"
+
+
+# Prepare info dictionary
+info_dict = {}
+
+# Load version -> the package's __version__.py module as a info dictionary
+if not VERSION:
+    # project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
+    # with open(os.path.join(path, project_slug, "__version__.py")) as f:
+    with open(os.path.join(path, NAME, '__version__.py')) as f:
+        exec(f.read(), info_dict)
+else:
+    info_dict["__version__"] = VERSION
+
+
+# Load README.md
+try:
+    with io.open(os.path.join(path, "README.md"), encoding="utf-8") as f:
+        info_dict["long_description"] = "\n" + f.read()
+except FileNotFoundError:
+    info_dict["long_description"] = DESCRIPTION
+
+
+# Load LICENSE
+with open("LICENSE") as f:
+    info_dict["license"] = f.read()
+
+
+# Load Packages required
+REQUIRED_DEFAULT = [
+    # N/A
+]
+
+# Load Package optional
+EXTRAS_DEFAULT = {
+    # N/A
+}
+
+
+class UploadCommand(Command):
+    """Support setup.py upload"""
+
+    description = "Build and publish the package"
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold"""
+        print("\033[1m{0}\033[0m".format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status("Removing previous builds ...")
+            rmtree(os.path.join(path, "dist"))
+        except OSError:
+            pass
+
+        self.status("Building Source and Wheel (universal) distribution…")
+        os.system("{0} setup.py sdist bdist_wheel --universal".format(sys.executable))
+
+        # self.status('Uploading the package to PyPI via Twine…')
+        # os.system('twine upload dist/*')
+
+        self.status("Pushing git tags…")
+        os.system("git tag v{0}".format(info_dict['__version__']))
+        os.system("git push --tags")
+
+        sys.exit()
+
 
 setup(
-    name="acme-core",
-    version="0.1",
-    description="xxx",
-    author="xxx",
-    author_email="xxx",
-    url="xxx",
-    packages=find_packages(where='src'),
-    package_dir={"": "src"},
-    install_requires=[i.strip() for i in open("requirements.txt").readlines()],
+    python_requires=REQUIRES_PYTHON,
+    name=NAME,
+    version=info_dict['__version__'],
+    description=DESCRIPTION,
+    long_description=info_dict['long_description'],
+    long_description_content_type="text/markdown",
+    author=AUTHOR,
+    author_email=EMAIL,
+    license=info_dict["license"],
+    url=URL,
+    package_dir={'': 'src'},
+    packages=find_packages("src", exclude=PACKAGES_EXCLUDE),
+    install_requires=[i.strip() for i in open(REQUIREMENTS_FILE).readlines()],
+    extras_require=EXTRAS_DEFAULT,
+    include_package_data=True,
     scripts=[],
-    test_suite="tests"
+    classifiers=[
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: Implementation :: Python",
+    ]
+    # $ setup.py publish support
+    # cmdclass={
+    #    'upload': UploadCommand,
+    # },
 )
