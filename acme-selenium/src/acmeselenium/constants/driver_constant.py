0e329c3b615461f8d14c680ca1159149123372fb
vjmadrid <vjmadrid@atsistemas.com>
2021-10-13 18:22:02 +0200

    Add new keys

--- acme-selenium/src/acmeselenium/constants/driver_constant.py
+++ acme-selenium/src/acmeselenium/constants/driver_constant.py
@@ -11,7 +11,11 @@ DEFAULT_CHROME_DRIVER_PATH_MAC_OS = "/usr/local/bin/chromedriver"
 
 DEFAULT_SAVE_TO_DISK_FILE_TYPES = "audio/mp3, text/plain"
 
+DRIVER_KEY = "driver"
+MAIN_WINDOW_KEY = "main_window"
 BROWSER_TYPE_KEY = "browser_type"
 HEADLESS_KEY = "headless"
 DOWNLOAD_PATH_KEY = "download_path"
 FILE_TYPES_KEY = "file_types"
+DELAY_KEY = "delay"
+RETRY_KEY = "retry"
